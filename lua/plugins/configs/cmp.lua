438ed345474082b74a831c264fcd19ce0eef8dc0
Julius Schumacher <julius.schumacher@kghcustoms.com>
2021-10-10 14:40:49 +0200

    some more changes

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -27,14 +27,7 @@ cmp.setup{
     },
     snippet = {
         expand = function(args)
-            -- For `vsnip` user.
-            -- vim.fn["vsnip#anonymous"](args.body)
-
-            -- For `luasnip` user.
             require('luasnip').lsp_expand(args.body)
-
-            -- For `ultisnips` user.
-            -- vim.fn["UltiSnips#Anon"](args.body)
         end,
     },
 }
---
+++ lua/plugins/configs/dap.lua
@@ -0,0 +1,18 @@
+local dap = require('dap')
+
+dap.adapters.netcoredbg = {
+    type = 'executable',
+    command = '/usr/sbin/netcoredbg',
+    args = {'--interpreter=vscode'}
+}
+
+dap.configurations.cs = {
+    {
+        type = "netcoredbg",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        end,
+    },
+}
